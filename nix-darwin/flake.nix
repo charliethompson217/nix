be4e0d720e373280c2cb1b68f1c57b236ac76182
Charles Thompson <80548854+charliethompson217@users.noreply.github.com>
2025-08-09 21:09:31 -0400

    Add package: gnupg

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -87,7 +87,7 @@
           };
 
           environment.systemPackages = with nixpkgs.legacyPackages.aarch64-darwin; [
-            git python3 nodejs tree htop rustup neovim ripgrep fzf rsync
+            git python3 nodejs tree htop rustup neovim ripgrep fzf rsync gnupg
           ];
 
           nix.settings.experimental-features = [ "nix-command" "flakes" ];
